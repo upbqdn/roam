b3621b6e00064ff93409188f08c947224ff77d63
Marek <mail@marek.onl>
2023-05-14 17:45:34 +0200

    Remove the test node

---
+++ .auctex-auto/ref.el
@@ -0,0 +1,30 @@
+(TeX-add-style-hook
+ "ref"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "gb"
+    "bb"
+    "iva"
+    "hh1"
+    "hh2"
+    "igb"
+    "gbs"
+    "fips"
+    "rijndael"
+    "shoup"
+    "lidl"
+    "menezes"
+    "cid05"
+    "bulygin"
+    "cid06"
+    "anf"
+    "shannon"
+    "sagemath"
+    "polybori"
+    "cryptominisat"
+    "arnold"
+    "aumasson"
+    "f4"
+    "fglm"))
+ '(or :bibtex :latex))
+
